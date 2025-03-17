1bc04306c020589e08291bfa1d49c3ab3cef29cb
Norvy <71366308+Rattonikus@users.noreply.github.com>
2025-03-16 19:45:50 -0600

    messing around with libusb in java

--- MavenProject/rkdriver/src/main/java/com/rattonikus/Controller.java
+++ MavenProject/rkdriver/src/main/java/com/rattonikus/Controller.java
@@ -12,8 +12,45 @@ public class Controller {
     public void start()
     {
         System.out.println("hello world");
-        startLibUsb();
+        System.out.println(findAllDevice());
     }
+    
+
+    private short findAllDevice()
+    {
+
+        //read device list 
+        Context context = new Context(); 
+        int resuult = LibUsb.init(context);
+
+        DeviceList list = new DeviceList(); 
+        int result = LibUsb.getDeviceList(context, list);
+        if (result < 0 ) throw new LibUsbException("unable to get list", result);
+
+
+        try 
+        {
+            for(Device device: list)
+            {
+                DeviceDescriptor descriptor = new DeviceDescriptor(); 
+                result = LibUsb.getDeviceDescriptor(device, descriptor);
+                if (result != LibUsb.SUCCESS) throw new LibUsbException("Unable to read descriptor", result);
+                //if (descriptor.idVendor() == vendorId && descriptor.idProduct() == productId) return device;
+                return descriptor.iProduct();
+                
+            }
+        }
+        finally
+        {
+            LibUsb.freeDeviceList(list, true);
+        }
+        return (Short) null; 
+    }
+    
+
+
+
+
 
     private void startLibUsb() 
     {
@@ -27,6 +64,52 @@ public class Controller {
         
         LibUsb.exit(context);
         System.out.println("Disconnect"); 
+    }
+
+    private int listDevice()
+    {
+        Context context = new Context(); 
+        int result = LibUsb.init(context);
+        DeviceList list = new DeviceList();
+        int endresult = LibUsb.getDeviceList(context, list);
+        for (Device device: list) {
+            DeviceDescriptor descriptor = new DeviceDescriptor(); 
+            result = LibUsb.getDeviceDescriptor(device, descriptor);
+            return result;
+        }
+        return 69; 
+    
+    }
+
+    private Device findDevice()
+    {
+
+        //read device list 
+        Context context = new Context(); 
+        int resuult = LibUsb.init(context);
+
+        DeviceList list = new DeviceList(); 
+        int result = LibUsb.getDeviceList(context, list);
+        if (result < 0 ) throw new LibUsbException("unable to get list", result);
+
+
+        try 
+        {
+            for(Device device: list)
+            {
+                DeviceDescriptor descriptor = new DeviceDescriptor(); 
+                result = LibUsb.getDeviceDescriptor(device, descriptor);
+                if (result != LibUsb.SUCCESS) throw new LibUsbException("Unable to read descriptor", result);
+                //if (descriptor.idVendor() == vendorId && descriptor.idProduct() == productId) return device;
+                return device;
+                
+            }
+        }
+        finally
+        {
+            LibUsb.freeDeviceList(list, true);
+        }
+        return null; 
     }
     
 
